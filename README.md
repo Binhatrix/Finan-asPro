994a3bc2c7a96778f335a0fd1c8b6e64b6e010b5
binhatrix <rezunho@hotmail.com>
2024-03-04 20:55:50 -0300

    Aula controller e erros API Rest

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Finan-asPro
+
+# Finanças Pro
+API dp projeto Finanças PRO - Controle de despesas pessoais
+
+## Requisitos
+
+- [] Crud de categorias
+- [] CRUD de movimentações
+- [] CRUD  de usuários
+- [] Autenticação
+- [] Dashboard
+
+## Documentação 
+
+### Endpoints
+
+VERBO + PATH (RECURSO)
+- [Listar Categorias](#listar-categorias)
+- Cadastrar Categoria
+- Detalhes da Categoria
+- Apagar categoria
+- Atualizar Categoria
+
+### Listar categorias
+
+SOAP - GranohQL - REST
+
+`GEtT / categoria
+
+
+
+
--- src/main/java/br/com/fiap/financaspro/Controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/Controller/CategoriaController.java
@@ -1,26 +1,52 @@
-package br.com.fiap.financaspro.Controller;
+package br.com.fiap.financaspro.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class CategoriaController {
-    @RequestMapping(method = RequestMapping.GET, path = "/categoria", produces = "application/json")
-    @ResponseBody
-    public String index(){
-    return """
-          [
-            {
-                "id":1,
-                "nome":"Alimentação",
-                "icone":"fast-food"
-            }
-            {
-               "id":2,
-                "nome":"Educação",
-                "icone":"book"
-            }
-            ]      
-             """;
+import br.com.fiap.financaspro.model.Categoria;
 
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+ 
+    Logger log = LoggerFactory.getLogger(getClass());
+    List<Categoria> repository = new ArrayList<>();
+ 
+    @GetMapping
+    public List<Categoria> listarCategorias() {
+        return repository;
+    }
+ 
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Categoria cadastrarCategoria(@RequestBody Categoria categoria) {
+        log.info("Cadastrando categoria: {}", categoria);
+        repository.add(categoria);
+        return categoria;
+    }
+   
+    @GetMapping("{id}")
+    public ResponseEntity<Categoria> mostraCategoria(@PathVariable Long id){
+        log.info("Buscando categoria por id {}", id);
+ 
+        for (Categoria categoria : repository) {
+            if (categoria.id().equals(id)) 
+                return ResponseEntity.ok(categoria);
+        }
+
+        //TODO refatorar com stream 
+ 
+        return ResponseEntity.notFound().build();
+    }
+ 
 }
