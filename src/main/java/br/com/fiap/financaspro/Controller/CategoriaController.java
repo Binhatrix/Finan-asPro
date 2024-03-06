3ba1771393b247c2fb7d4666b63117d74ea9562f
binhatrix <rezunho@hotmail.com>
2024-03-05 22:47:41 -0300

    Update API

--- src/main/java/br/com/fiap/financaspro/Controller/CategoriaController.java
+++ src/main/java/br/com/fiap/financaspro/Controller/CategoriaController.java
@@ -2,31 +2,37 @@ package br.com.fiap.financaspro.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financaspro.model.Categoria;
-
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
- 
+
     Logger log = LoggerFactory.getLogger(getClass());
     List<Categoria> repository = new ArrayList<>();
- 
+
     @GetMapping
     public List<Categoria> listarCategorias() {
         return repository;
     }
- 
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Categoria cadastrarCategoria(@RequestBody Categoria categoria) {
@@ -34,19 +40,74 @@ public class CategoriaController {
         repository.add(categoria);
         return categoria;
     }
-   
+
     @GetMapping("{id}")
-    public ResponseEntity<Categoria> mostraCategoria(@PathVariable Long id){
+    public ResponseEntity<Categoria> mostraCategoria(@PathVariable Long id) {
         log.info("Buscando categoria por id {}", id);
- 
-        for (Categoria categoria : repository) {
-            if (categoria.id().equals(id)) 
-                return ResponseEntity.ok(categoria);
+
+        // for (Categoria categoria : repository) {
+        // if (categoria.id().equals(id))
+        // return ResponseEntity.ok(categoria);
+        // }
+        // TODO refatorar com stream
+
+        var categoriaEncontrada = getCategoriaById(id);
+
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+
         }
 
-        //TODO refatorar com stream 
- 
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(categoriaEncontrada.get());
+
     }
- 
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("apagando categoria");
+
+        var categoriaEncontrada = getCategoriaById(id);
+        
+        if (categoriaEncontrada.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        repository.remove(categoriaEncontrada.get());
+        
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria){
+        log.info("Atualizando categoria com id {} para {}", id, categoria);
+        //buscar categoria
+        var categoriaEncontrada = getCategoriaById(id);
+        if (categoriaEncontrada.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        //criar uma nova categoria com dados atuais
+        var categoriaAntiga = categoriaEncontrada.get();
+        var categoriaNova = new Categoria(id, categoria.nome(), categoria.icone());
+        
+        // apagar categoria antiga
+        repository.remove(categoriaAntiga);
+        
+        //adicionar categoria nova
+        repository.add(categoriaNova);
+
+        return ResponseEntity.ok(categoriaNova);
+    }
+
+
+
+
+
+
+    private Optional<Categoria> getCategoriaById(Long id) {
+        var categoriaEncontrada = repository
+        .stream()
+        .filter(c -> c.id().equals(id))
+        .findFirst();
+        return categoriaEncontrada;
+    }
 }
